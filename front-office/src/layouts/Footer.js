e7104ce6b543b01b1fd5f4d7d9e2a5b2a1a6fe31
Ghofrane266 <155783091+Ghofrane266@users.noreply.github.com>
2024-07-03 01:03:35 +0200

    50% of work

--- front-office/src/layouts/Footer.js
+++ front-office/src/layouts/Footer.js
@@ -1,10 +1,42 @@
 import React from 'react'
-
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaLocationArrow,
+  FaMobileAlt,
+} from "react-icons/fa";
 function Footer() {
   return (
-    <div>
-      Footer
-    </div>
+    <div className="rounded-t-3xl" style={{ background: '#393d40' }}>
+      <div className="container">
+        
+        <div className="grid md:grid-cols-3 py-4">
+            {/* company Details */}
+            <div className="py-8 px-4">
+              <h1 className="sm:text-3xl text-xl  sm:text-left text-justify mb-3 flex items-center gap-3 font-bold" style={{ color: '#b8d941' }}>
+              Univers-Automatisme
+              </h1>
+              <p className="text-sm" style={{ color: '#f2f2f2' }}>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Repellat dolor illum a aut iste expedita.
+              </p>
+              <br />
+              <div>
+                <div className="flex items-center gap-3">
+                  <FaLocationArrow style={{ color: '#b8d941' }}/>
+                  <p style={{ color: '#eaf2ae' }}>Tunisia,tunisia</p>
+                </div>
+                <div className="flex items-center gap-3">
+                  <FaMobileAlt style={{ color: '#b8d941' }}/>
+                  <p style={{ color: '#eaf2ae' }}>+216 123456789</p>
+                </div>
+              </div>
+              </div>
+        </div>
+      </div>
+      
+          </div>
   )
 }
 
--- front-office/src/layouts/Header.js
+++ front-office/src/layouts/Header.js
@@ -1,13 +1,71 @@
 "use client";
+import Image from 'next/image'
+import logounivers from '../images/logounivers.png'
+import "../styles/globals.css"
+import Link from 'next/link';
+import users from '../images/users.png'
 
 
 const Header = () => {
- 
+
   return (
-  <div>
-    Header
-  </div>
-  );
-};
+
+
+    <section >
+      <nav  >
+        <div className="w-screen flex flex-wrap items-center justify-between mx-auto p-2" style={{ background: '#f2f2f2' }}>
+          <div className="flex items-center gap-3">
+            <Image
+              src={logounivers}
+              width={100}
+              height={85}
+              alt="logo-univers"
+
+            />
+            <span className="self-center text-xl font-bold whitespace-nowrap " style={{ color: '#393d40' }}>Univers-Automatisme</span>
+
+          </div>
+
+          <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+
+            <button data-collapse-toggle="navbar-solid-bg" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm  rounded-lg md:hidden  focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-solid-bg" aria-expanded="false">
+              <span className="sr-only">Open main menu</span>
+              <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+              </svg>
+            </button>
+            <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+              <ul className="flex flex-col font-semibold mt-4 text-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0   px-5">
+                <li>
+                  <Link href="/" className="block py-2 px-3 md:p-0 " aria-current="page" style={{ color: '#393d40' }}>Home</Link>
+                </li>
+                <li>
+                  <Link href="/about" className="block py-2 px-3 md:p-0  " style={{ color: '#393d40' }}>About</Link>
+                </li>
+
+                <li>
+                  <Link href="/contact" className="block py-2 px-3 md:p-0  " style={{ color: '#b8d941' }}>Contact</Link>
+                </li>
+                <li>
+                  
+                    <Image className="w-9 h-9 rounded-full" src={users} alt="user photo" />
+                 
+                </li>
+              </ul>
+            </div>
+
+
+
+
+          </div>
+        </div>
+      </nav>
+      <div>
+        header
+      </div>
+    </section>
+  )
+}
+
 
 export default Header;
