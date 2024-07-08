216d1c7dc2ca64ac5f02522c11f37f2ad75cfef7
rawen10 <rawensoltqni@gmail.com>
2024-07-08 15:32:50 +0200

    adding login

--- back-office/src/App.jsx
+++ back-office/src/App.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
-import { Outlet } from "react-router-dom";
 import "./App.css";
-import DrawerLeft from "./layout/Drawer";
-import Footer from "./layout/Footer";
-import Navbar from "./layout/Navbar";
 import Sidebar from "./layout/Sidebar";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Main from './layout/Sidebar';
 
 function App() {
   const [open, setOpen] = React.useState(false);
   return (
     <div className="App">
-      {/* <Navbar /> */}
-      {/* <DrawerLeft /> */}
       <Sidebar  />
-      {/* <Footer/> */}
     </div>
   );
 }
--- back-office/src/Pages/SignUp.jsx
+++ back-office/src/Pages/SignUp.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import '../App.css'; // Assurez-vous que votre fichier CSS est correctement importé
+// import 'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css';
+import logo from "../assets/logo.png";
 
 function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <div>
+      <div className="circle"></div>
+      <div className="card">
+        <div className="logo">
+          <img src={logo} alt="Logo" className="logo-image" />
+        </div>
+        <h2>Create Account</h2>
+        <form className="form">
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button type="submit">Sign Up</button>
+        </form>
+        <footer>
+          Existing users, Log In
+          <a href="/">Here</a>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
-export default SignUp
+export default SignUp;
